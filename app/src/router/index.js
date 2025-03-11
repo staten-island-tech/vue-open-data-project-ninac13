9011ae95df45b2f73635d1a6ab359bd77be799fe
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-03-11 13:40:04 -0400

    3/11

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -18,8 +18,8 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue'),
     },
     {
-      path: "/restaurant/:id",
-      name: "restaurantData",
+      path: "/rating",
+      name: "RatingGraph",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
