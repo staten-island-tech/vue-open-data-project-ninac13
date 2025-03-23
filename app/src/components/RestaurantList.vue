dcc3ea9b4c72708435cd56cf82114c30698f9968
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-03-23 14:28:04 -0400

    3/23

--- app/src/components/RestaurantList.vue
+++ app/src/components/RestaurantList.vue
@@ -1,10 +1,16 @@
 <script setup>
-import Restaurants from "../components/RestaurantCards.vue"
-const restaurants = ref("");
+import {ref, onMounted} from 'vue';
+import Restaurants from "./RestaurantCards.vue";
+const restaurants = ref([]);
+
 async function getRestaurantData() {
-  let res = await fetch("https://data.cityofnewyork.us/resource/43nn-pn8j.json");
-  let data = await res.json();
-  restaurants.value = data;
+    try {
+        let res = await fetch("https://data.cityofnewyork.us/resource/43nn-pn8j.json");
+        let data = await res.json();
+        restaurants.value = data;
+    } catch (error) {
+        console.log("There was an error", error);
+    }
 }
 onMounted(()=>{
   getRestaurantData();
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -2,11 +2,16 @@
     <div class="container">
     <button @click="goToRating">Ratings Per ZipCode</button>
     <button @click="goToPie">Restaurant Percentage Per Borough</button>
+    <RestaurantList />
   </div>
 </template>
 
 <script>
+import RestaurantList from '../components/RestaurantList.vue';
 export default {
+  components: {
+    RestaurantList,
+  },
   methods: {
     goToRating() {
       this.$router.push('/rating')
