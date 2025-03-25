c43291b53d53eb7f29537010d6f34c96f059cdbf
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-03-25 17:08:19 -0400

    3/25

--- app/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1 class="flex justify-center text-base-content text-5xl mx-auto w-fit px-4 py-4 bg-base-100 border-neutral border-4 mb-5 underline underline-offset-8 rounded-3xl shadow-2xl">NYC Restaurant Inspections</h1>
-  <nav>
-    <RouterLink to="/">Go Back to Home</RouterLink>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
-</template>
