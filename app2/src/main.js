bdde47b4c497bac0bef979fdd934bf8ac13b6ddc
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-03-25 11:43:18 -0400

    3/25

--- app2/src/main.js
+++ app2/src/main.js
@@ -2,8 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '../../app2/src/router'
-
+import router from './router'
 
 const app = createApp(App)
 
